74c8e37542a3bfe75fc167bf26e248754cdcef55
PedroSousaCoelho <pedrosousacoelhoo@gmail.com>
2022-10-08 20:33:51 -0300

    att. 02

--- pages/index.js
+++ pages/index.js
@@ -101,7 +101,7 @@ export default function Home() {
             </p>
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-            <div className="basis-1/3 flex-1"><Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={calculadora}/></div>
+            <div className="basis-1/3 flex-1"><a href='https://pedrosousacoelho.github.io/calculadora/'><Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={calculadora}/></a></div>
             <div className="basis-1/3 flex-1"><Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={web2}/></div>
             <div className="basis-1/3 flex-1"><Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={web3}/></div>
             <div className="basis-1/3 flex-1"><Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={web4}/></div>
