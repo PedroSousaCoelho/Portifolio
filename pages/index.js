9804cea77d5171f67537749524c807c456ad595a
PedroSousaCoelho <pedrosousacoelhoo@gmail.com>
2022-11-07 23:26:38 -0300

    Add Project

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ import web4 from "../public/web4.png";
 import web5 from "../public/web5.png";
 import web6 from "../public/web6.png";
 import calculadora from "../public/calculadora.png";
+import imc from "../public/imc.png"
 import port from "../public/port.png"
 import { useState } from 'react';
 
@@ -104,7 +105,7 @@ export default function Home() {
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
             <div className="basis-1/3 flex-1"><a href='https://pedrosousacoelho.github.io/calculadora/'><Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={calculadora}/></a></div>
             <div className="basis-1/3 flex-1"><a href='https://pedrosousacoelho.github.io/Formulario/'><Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={port}/></a></div>
-            <div className="basis-1/3 flex-1"><Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={web3}/></div>
+            <div className="basis-1/3 flex-1"><a href='https://pedrosousacoelho.github.io/imc/'><Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={imc}/></a></div>
             <div className="basis-1/3 flex-1"><Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={web4}/></div>
             <div className="basis-1/3 flex-1"><Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={web5}/></div>
             <div className="basis-1/3 flex-1"><Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={web6}/></div>
